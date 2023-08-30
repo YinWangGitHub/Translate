53aef172dbaabb25c356314daf3b77e89744e7b5
chalia082 <59096707+chalia082@users.noreply.github.com>
2023-08-30 15:21:20 -0500

    deleted files

--- credential_check.py
+++
@@ -1,7 +0,0 @@
-import automationassets
-
-print (automationassets.get_automation_variable("myvariable"))
-
-cred = automationassets.get_automation_credential("mycredential")
-print (cred['username'])
-print (cred['password'])
